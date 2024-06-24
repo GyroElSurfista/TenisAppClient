102d5c0aa347896862c3067c1f6bed24066b69bb
Jairo Maida <gyroelsurfista@gmail.com>
2024-06-23 22:10:27 -0400

    feat: port as parameter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PlayerModule],
+  imports: [PlayerModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
